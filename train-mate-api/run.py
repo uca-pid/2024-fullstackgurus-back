afec06772dae0ec0064e79502835f10aee7dbb5e
SebaDere <dere1997@gmail.com>
2024-09-11 17:51:26 -0300

    testing cors

--- train-mate-api/run.py
+++ train-mate-api/run.py
@@ -3,7 +3,7 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*", "allow_headers": ["Authorization", "Content-Type"]}})
 
 
 if __name__ == '__main__':
