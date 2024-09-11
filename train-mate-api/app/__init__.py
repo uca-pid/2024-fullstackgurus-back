ad7b7c08f304d09bc61efc06ed6eb6dbf4ebe3c7
SebaDere <dere1997@gmail.com>
2024-09-11 18:14:57 -0300

    fix cors

--- train-mate-api/app/__init__.py
+++ train-mate-api/app/__init__.py
@@ -1,8 +1,11 @@
 # app/__init__.py
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     # Importar y registrar los blueprints (controladores)
     from app.controllers.user_controller import user_bp
--- train-mate-api/run.py
+++ train-mate-api/run.py
@@ -1,10 +1,8 @@
 # run.py
 from app import create_app
-from flask_cors import CORS
+
 
 app = create_app()
-CORS(app, resources={r"/*": {"origins": "*", "allow_headers": ["Authorization", "Content-Type"]}})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
