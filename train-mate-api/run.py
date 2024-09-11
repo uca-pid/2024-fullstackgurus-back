3d2f91fe07c8fca93f7a8c1130fedca9303d014c
SebaDere <dere1997@gmail.com>
2024-09-10 23:10:22 -0300

    cors

--- train-mate-api/run.py
+++ train-mate-api/run.py
@@ -3,7 +3,7 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, resources={r"/*": {"origins": "https://2024-fullstackgurus-front.vercel.app"}})
+CORS(app)
 
 
 if __name__ == '__main__':
